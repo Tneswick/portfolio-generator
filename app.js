d5c9d6904a8ea1b1f940342af8c7f9f2f592c615
Deck Stream <tneswick2@verizon.net>
2022-03-21 10:58:17 -0700

    this is a test commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var variable = "this is a whole other string"
+
+console.log(`french bread is "good" ${variable}`)
+
+variable == "this is a hole other string" ? console.log("true") : console.log("false");
